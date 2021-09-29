8d5859e27225b6d34c22c4ebc811170dbbf250e1
Guillaume40480 <87611658+Guillaume40480@users.noreply.github.com>
2021-09-29 11:58:19 +0200

    gitignore crée incluant node_modules/

---
+++ wilders.js
@@ -0,0 +1,11 @@
+const chalk = require('chalk');
+
+const wilder1 = "micka";
+const wilder2 = "greg";
+const wilder3 = "emilie";
+const wilder4 = "marie";
+
+console.log(chalk.yellow(wilder1));
+console.log(chalk.green(wilder2));
+console.log(chalk.blue(wilder3));
+console.log(chalk.magenta(wilder4));
